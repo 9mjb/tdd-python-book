87b75845e376ea107a22d79867b806bf06013e4c
9mjb <MikeBrown9mjb@gmail.com>
2020-09-23 16:30:27 -0500

    functional_tests.py into functional_tests/tests.py and some migrations

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -25,8 +25,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
-        
-        
+
+
     # #######################################################
     def test_can_start_a_list_and_retreive_it_later(self):
         # """ test* functions are tests... """
