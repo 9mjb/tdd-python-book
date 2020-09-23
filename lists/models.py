83cd8cdac27f79379ce04ff758b037b59f39c70d
9mjb <MikeBrown9mjb@gmail.com>
2020-09-23 08:47:00 -0500

    model for list Item and assoc migration

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+# manage.py test
+# django.db.utils.OperationalError: no such column: lists_item.text
+# manage.py makemigrations
+
+class Item(models.Model):
+    text = models.TextField(default='')
--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,6 @@
+# tests.py
+# mjb 2020/09/22
+
 from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
@@ -5,6 +8,30 @@ from django.template.loader import render_to_string
 
 from lists.views import home_page  # (ff views.py)
 
+# (ff https://docs.DjangoProject.com/en/1.11/intro/tutorial01/)
+from lists.models import Item # (ff models.py)
+
+# #######################################################
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retreiving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item The second'
+        second_item.save()
+        
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item The second')
+
+# #######################################################
 class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
@@ -32,6 +59,8 @@ class HomePageTest(TestCase):
 
 
 
+# #######################################################
 #class SmokeTest(TestCase):
 #    def test_bad_math(self):
 #        self.assertEqual(1+1, 3)
+
